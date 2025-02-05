883c4631f384a0bcd3b3fbc5e4cc89d23b9307d4
Nathan Pagnucco <nathan.p3pro@gmail.com>
2025-02-05 16:21:10 +0100

    Commit initial

---
+++ src/main/java/fr/npagnucco/projetspring/ProjetspringApplication.java
@@ -0,0 +1,13 @@
+package fr.npagnucco.projetspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetspringApplication.class, args);
+	}
+
+}
