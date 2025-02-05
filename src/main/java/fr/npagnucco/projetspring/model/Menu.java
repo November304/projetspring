2817efeaedfcff993b820befd0d43b7cd3b535d5
Nathan Pagnucco <nathan.p3pro@gmail.com>
2025-02-05 17:01:45 +0100

    Création des models et parametrage

---
+++ src/main/java/fr/npagnucco/projetspring/model/Menu.java
@@ -0,0 +1,87 @@
+package fr.npagnucco.projetspring.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+
+/**
+ * Entité qui représente un menu en base de données.
+ * @author nathan
+ */
+@Entity(name="menu")
+public class Menu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nom;
+    private double prix;
+    private String description;
+
+    @ManyToMany
+    @JoinTable(name = "menu_plat",
+        joinColumns=@JoinColumn(name="menu_id",referencedColumnName="id"),
+        inverseJoinColumns=@JoinColumn(name="plat_id",referencedColumnName="id")
+    )
+    private List<Plat> plats;
+
+    public Menu() {
+    }
+
+    public Menu(String description, String nom, List<Plat> plats, double prix) {
+        this.description = description;
+        this.nom = nom;
+        this.plats = plats;
+        this.prix = prix;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Plat> getPlats() {
+        return plats;
+    }
+
+    public void setPlats(List<Plat> plats) {
+        this.plats = plats;
+    }
+
+
+
+
+
+}
---
+++ src/main/java/fr/npagnucco/projetspring/model/Plat.java
@@ -0,0 +1,112 @@
+package fr.npagnucco.projetspring.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
+
+/**
+ * Entité qui représente un plat en base de données.
+ * @author nathan
+ */
+@Entity(name="Plat")
+public class Plat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nom;
+    private int nb_calories;
+    private int nb_glucides;
+    private int nb_lipides;
+    private int nb_proteines;
+
+    @OneToOne
+    @JoinColumn(name = "categorie_id")
+    private Categorie categorie;
+
+    @ManyToMany(mappedBy = "plats")
+    private List<Menu> menus;
+
+    public Plat() {
+    }
+
+    public Plat(Categorie categorie, List<Menu> menus, int nb_calories, int nb_glucides, int nb_lipides, int nb_proteines, String nom) {
+        this.categorie = categorie;
+        this.menus = menus;
+        this.nb_calories = nb_calories;
+        this.nb_glucides = nb_glucides;
+        this.nb_lipides = nb_lipides;
+        this.nb_proteines = nb_proteines;
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getNb_calories() {
+        return nb_calories;
+    }
+
+    public void setNb_calories(int nb_calories) {
+        this.nb_calories = nb_calories;
+    }
+
+    public int getNb_glucides() {
+        return nb_glucides;
+    }
+
+    public void setNb_glucides(int nb_glucides) {
+        this.nb_glucides = nb_glucides;
+    }
+
+    public int getNb_lipides() {
+        return nb_lipides;
+    }
+
+    public void setNb_lipides(int nb_lipides) {
+        this.nb_lipides = nb_lipides;
+    }
+
+    public int getNb_proteines() {
+        return nb_proteines;
+    }
+
+    public void setNb_proteines(int nb_proteines) {
+        this.nb_proteines = nb_proteines;
+    }
+
+    public Categorie getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(Categorie categorie) {
+        this.categorie = categorie;
+    }
+
+    public List<Menu> getMenus() {
+        return menus;
+    }
+
+    public void setMenus(List<Menu> menus) {
+        this.menus = menus;
+    }
+}
